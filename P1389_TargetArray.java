66f768163e6884b6c5495b577d559e08eb091412
AamulSri <aamul24.vns@gmail.com>
2023-12-31 18:56:53 +0530

    LeetCode Problem 1389

---
+++ P1389_TargetArray.java
@@ -0,0 +1,20 @@
+package LeetCodeSolutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class P1389_TargetArray{
+   public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<Integer>();
+        int size = nums.length;
+        int[] target = new int[size];
+        for(int i=0;i<size;i++){
+            list.add(index[i],nums[i]);
+        }
+    
+        for(int i=0;i<size;i++){
+            target[i]=list.get(i);
+        }
+        return target;
+    }
+}
